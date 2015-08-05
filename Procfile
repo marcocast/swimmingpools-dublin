ce6dbfa9d3f4b5a5c865a452a9b2e6af1aae47db
marcocast <marcocast@gmail.com>
2015-08-05 17:15:07 +0200

    init

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS  -Dappname=appname_in_heroku -Dservice.port=$PORT -cp target/classes:target/dependency/* com.ucd.geoservices.app.Main
+web: java $JAVA_OPTS -Dservice.port=$PORT -cp target/classes:target/dependency/* com.ucd.geoservices.app.Main
--- src/main/java/com/ucd/geoservices/app/Main.java
+++ src/main/java/com/ucd/geoservices/app/Main.java
@@ -1,13 +1,21 @@
 package com.ucd.geoservices.app;
 
+import java.util.Optional;
+
 import com.aol.micro.server.MicroserverApp;
 import com.aol.micro.server.config.Microserver;
 
 @Microserver(basePackages = { "com.ucd.geoservices" })
 public class Main {
+	
+	public static final String APPNAME = Optional.ofNullable(
+			System.getenv("appname")).orElse(
+			System.getProperty("appname"));
 
 	public static final GeoservicesModule GEOSERVICES_MODULE = new GeoservicesModule(
-			"geoservices/resources");
+			APPNAME + "/resources");
+	
+	
 
 	public static void main(String[] args) {
 
