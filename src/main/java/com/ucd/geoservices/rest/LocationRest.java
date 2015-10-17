7848b1258a7eccd8fd32241fb806a19431d82926
Marco Castigliego <marcocast@gmail.com>
2015-10-17 16:31:04 +0200

    formatted test

--- src/main/java/com/ucd/geoservices/rest/LocationRest.java
+++ src/main/java/com/ucd/geoservices/rest/LocationRest.java
@@ -29,7 +29,7 @@ public class LocationRest {
 
 	@GET
 	@Produces("text/plain")
-	@Path("/info")
+	@Path("/infoooo")
 	public Response info() {
 		return Response.ok("Some info here").build();
 	}
@@ -39,12 +39,9 @@ public class LocationRest {
 	@Consumes("application/json")
 	@Produces("application/json")
 	public Response queryWithRadius(final String queryRequestJson) {
-		QueryRadiusRequest queryRequest = JacksonUtil.convertFromJson(
-				queryRequestJson, QueryRadiusRequest.class);
+		QueryRadiusRequest queryRequest = JacksonUtil.convertFromJson(queryRequestJson, QueryRadiusRequest.class);
 
-		return Response.ok(
-				JacksonUtil.serializeToJson(geoService
-						.queryWithRadius(queryRequest))).build();
+		return Response.ok(JacksonUtil.serializeToJson(geoService.queryWithRadius(queryRequest))).build();
 	}
 
 	@POST
@@ -52,12 +49,9 @@ public class LocationRest {
 	@Consumes("application/json")
 	@Produces("application/json")
 	public Response queryWithBoundaries(final String queryRequestJson) {
-		QueryBoundariesRequest queryRequest = JacksonUtil.convertFromJson(
-				queryRequestJson, QueryBoundariesRequest.class);
+		QueryBoundariesRequest queryRequest = JacksonUtil.convertFromJson(queryRequestJson, QueryBoundariesRequest.class);
 
-		return Response.ok(
-				JacksonUtil.serializeToJson(geoService
-						.queryWithBoundaries(queryRequest))).build();
+		return Response.ok(JacksonUtil.serializeToJson(geoService.queryWithBoundaries(queryRequest))).build();
 	}
 
 	@POST
@@ -65,12 +59,9 @@ public class LocationRest {
 	@Consumes("application/json")
 	@Produces("application/json")
 	public Response queryWithAddress(final String queryAddressRadiusRequest) {
-		QueryAddressRadiusRequest queryRequest = JacksonUtil.convertFromJson(
-				queryAddressRadiusRequest, QueryAddressRadiusRequest.class);
+		QueryAddressRadiusRequest queryRequest = JacksonUtil.convertFromJson(queryAddressRadiusRequest, QueryAddressRadiusRequest.class);
 
-		return Response.ok(
-				JacksonUtil.serializeToJson(geoService
-						.queryWithAddressAndRadius(queryRequest))).build();
+		return Response.ok(JacksonUtil.serializeToJson(geoService.queryWithAddressAndRadius(queryRequest))).build();
 	}
 
 	@POST
@@ -78,12 +69,9 @@ public class LocationRest {
 	@Consumes("application/json")
 	@Produces("application/json")
 	public Response add(final String locationJson) {
-		Location location = JacksonUtil.convertFromJson(locationJson,
-				Location.class);
+		Location location = JacksonUtil.convertFromJson(locationJson, Location.class);
 
-		return Response.ok(
-				JacksonUtil.serializeToJson(geoService.addLocation(location)))
-				.build();
+		return Response.ok(JacksonUtil.serializeToJson(geoService.addLocation(location))).build();
 	}
 
 	@POST
@@ -91,11 +79,8 @@ public class LocationRest {
 	@Consumes("application/json")
 	@Produces("application/json")
 	public Response addwithAddress(final String addressJson) {
-		PlainAddress plainAddress = JacksonUtil.convertFromJson(addressJson,
-				PlainAddress.class);
-		return Response.ok(
-				JacksonUtil.serializeToJson(geoService
-						.addLocation(plainAddress))).build();
+		PlainAddress plainAddress = JacksonUtil.convertFromJson(addressJson, PlainAddress.class);
+		return Response.ok(JacksonUtil.serializeToJson(geoService.addLocation(plainAddress))).build();
 	}
 
 	@POST
@@ -103,12 +88,9 @@ public class LocationRest {
 	@Consumes("application/json")
 	@Produces("application/json")
 	public Response remove(final String locationJson) {
-		Location location = JacksonUtil.convertFromJson(locationJson,
-				Location.class);
+		Location location = JacksonUtil.convertFromJson(locationJson, Location.class);
 
-		return Response
-				.ok(JacksonUtil.serializeToJson(geoService
-						.removeLocation(location))).build();
+		return Response.ok(JacksonUtil.serializeToJson(geoService.removeLocation(location))).build();
 	}
 
 }
