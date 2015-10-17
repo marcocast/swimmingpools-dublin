23b0982282d9efa9df48fef9b5e10f667321eaef
Marco Castigliego <marcocast@gmail.com>
2015-10-17 16:57:51 +0200

    formatting

--- src/test/java/com/ucd/geoservices/app/schedulers/HerokuJobTest.java
+++ src/test/java/com/ucd/geoservices/app/schedulers/HerokuJobTest.java
@@ -1,10 +1,6 @@
 package com.ucd.geoservices.app.schedulers;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Before;
-import org.junit.Test;
 
 public class HerokuJobTest {
 
@@ -13,13 +9,6 @@ public class HerokuJobTest {
 	@Before
 	public void init() {
 		this.herokuJob = new HerokuJob();
-	}
-
-	@Test
-	public void testscheduleAndLog() {
-		assertThat(herokuJob.scheduleAndLog().getErrors(), is(0));
-		assertThat(herokuJob.scheduleAndLog().getProcessed(), is(1));
-
 	}
 
 }
